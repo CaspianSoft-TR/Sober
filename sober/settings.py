2998c3e4cae2d5bae59810f1aa1bd19d4c62e16d
Musadiq Aliyev <musadiq.aliyev@gmail.com>
2019-02-04 18:20:56 +0400

    Update settings.py

--- sober/settings.py
+++ sober/settings.py
@@ -144,7 +144,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'sober/static')
+]
 SITE_ID = 1
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
