c74eb754d1fef7976562b2e4f53003ea1cc230c2
Kemal S. KARACA (ULAKBIM) <kemalsami.karaca@tubitak.gov.tr>
2019-02-10 14:46:08 +0300

    PULL **

--- api/urls.py
+++ api/urls.py
@@ -11,9 +11,10 @@ from .views import (
 
 urlpatterns = [
 
+    path('register/', views.RegisterView.as_view()),
+
     path('car/create', CarCreateAPIView.as_view() , name='car-add'),
     path('car/', CarListAPIView.as_view() , name='car-list'),
-    path('register/', views.RegisterView.as_view()),
 
     path('user/car/create', UserCarCreateAPIView.as_view() , name='user-car-add'),
     path('user/car/list-all', UserCarListAllAPIView.as_view() , name='user-car-list-all'),
@@ -22,6 +23,7 @@ urlpatterns = [
     path('user/address/delete/<int:pk>/', views.UserAddressDeleteAPIView.as_view() , name='user-address-delete'),
     path('user/address/list/', views.UserAddressListAPIView.as_view() , name='user-address-list-owner'),
     path('user/address/list-all/', views.UserAddressListAllAPIView.as_view() , name='user-address-list-all'),
+
     path('booking/new', views.BookingCreateAPIView.as_view() , name='booking-new'),
     path('booking/list', views.BookingListAPIView.as_view() , name='booking-new'),
     path('booking/cancel/', views.BookingCancelAPIView.as_view() , name='booking-cancel'),
