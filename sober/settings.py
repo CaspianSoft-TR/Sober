1fd61e48e4537c8f27a4b3eb901c68c41da214e5
Musadiq Aliyev <musadiq.aliyev@gmail.com>
2019-02-13 15:28:09 +0400

    Update settings.py

--- sober/settings.py
+++ sober/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'sober.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -104,7 +104,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
